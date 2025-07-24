03127069143e1ceed98ad62a9a90ba6fe3a7ab31
Ajinkya8797 <ajinkyalandge10@gmail.com>
2025-07-25 00:22:50 +0530

    Sp1

---
+++ src/star_patterns/Sp1.java
@@ -0,0 +1,15 @@
+package star_patterns;
+
+public class Sp1 {
+
+	public static void main(String[] args) {
+		for(int i=1 ;i<=5 ;i++) {
+			for(int j=1 ;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
