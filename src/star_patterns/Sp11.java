0ed9ffe5205cfc18c56a6312386a1725358e7e54
Ajinkya8797 <ajinkyalandge10@gmail.com>
2025-08-17 10:50:08 +0530

    Sp11-12 Added

---
+++ src/star_patterns/Sp11.java
@@ -0,0 +1,15 @@
+package star_patterns;
+
+public class Sp11 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 1; j <= 4; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
