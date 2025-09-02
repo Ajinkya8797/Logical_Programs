93eeb6e655d012c3700a8b6187457d32177000d1
Ajinkya8797 <ajinkyalandge10@gmail.com>
2025-09-02 09:01:55 +0530

    Np4

---
+++ src/number_Patterns/Np4.java
@@ -0,0 +1,15 @@
+package number_Patterns;
+
+public class Np4 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = i; j >= 1; j--) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
